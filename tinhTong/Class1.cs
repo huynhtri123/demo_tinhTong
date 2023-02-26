d90a7520c87d0b348d96c75634644da9e44fba62
huynhtri123 <huynhmtri1211@gmail.com>
2023-02-26 19:30:08 +0700

    them khoc

---
+++ tinhTong/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tinhTong
+{
+    internal class Class1
+    {
+        public void cuoi()
+        { Console.WriteLine("haha");
+        }
+        public void hoi()
+        {
+            Console.WriteLine("Cuoi gi?");
+        }
+        public void hoi2()
+        {
+            Console.WriteLine("Cuoi gi vay");
+        }
+        public void traLoi()
+        {
+            Console.WriteLine("Mac cuoi");
+        }
+        public void traLoi2()
+        {
+            Console.WriteLine("OK");
+        }
+        public void khoc()
+        {
+            Console.WriteLine("hu hu");
+        }
+    }
+}
